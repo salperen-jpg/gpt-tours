9c197a8e4be5bffe52fba2723342fe6777fc6430
Salih Alperen <sal.alperen1@gmail.com>
2024-04-23 23:16:53 +0200

    Add basic controllers functionality

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -1,28 +1,37 @@
+import Tour from "../models/tourModel.js";
 import { StatusCodes } from "http-status-codes";
 
 // GET TOURS
 const getAllTours = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "AllTours" });
+  const tours = await Tour.find({});
+  res.status(StatusCodes.OK).json({ tours });
 };
 
 // POST TOUR
 const createTour = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ msg: "Created" });
+  await Tour.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: "Created successfully!" });
 };
 
 // GET TOUR
 const getTour = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "Tour" });
+  const tour = await Tour.findById(req.params.id);
+  res.status(StatusCodes.OK).json({ tour });
 };
 
 // UPDATE TOUR
 const updateTour = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "Updated" });
+  const newTourBody = req.body;
+  await Tour.findByIdAndUpdate(req.params.id, newTourBody, {
+    new: true,
+  });
+  res.status(StatusCodes.OK).json({ msg: "Updated successfully!" });
 };
 
 // DELETE TOUR
 const deleteTour = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "Tour" });
+  await Tour.findByIdAndDelete(req.params.id);
+  res.status(StatusCodes.OK).json({ msg: "Deleted successfully!" });
 };
 
 export { getAllTours, createTour, getTour, updateTour, deleteTour };
