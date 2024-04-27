7414afd33ef2e184f537d73e6b2a1d78f3613e7b
Salih Alperen <sal.alperen1@gmail.com>
2024-04-27 22:47:43 +0200

    Remove log

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -9,6 +9,5 @@ export const createJWT = (payload) => {
 
 export const verifyJWT = (token) => {
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  console.log(decoded);
   return decoded;
 };
