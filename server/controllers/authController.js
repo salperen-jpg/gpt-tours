261c9672d7f48b542cefb3a3860e500e278cafc0
Salih Alperen <sal.alperen1@gmail.com>
2024-04-27 14:30:36 +0200

    Add attach cookie functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,6 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
 import { UnauthenticatedError } from "../errors/customErrors.js";
-import jwt from "jsonwebtoken";
 import { createJWT } from "../utils/jwt.js";
 
 export const register = async (req, res) => {
@@ -12,20 +11,27 @@ export const register = async (req, res) => {
   await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ msg: "User created!" });
 };
+
 export const login = async (req, res) => {
-  const { password } = req.body;
   const loggedInUser = await User.findOne({ email: req.body.email });
   // check if user exists and passwords are matching
   const isUserValid =
-    loggedInUser && (await comparePassword(password, loggedInUser.password));
+    loggedInUser &&
+    (await comparePassword(req.body.password, loggedInUser.password));
   if (!isUserValid) throw new UnauthenticatedError("invalid credentials");
 
   // attach cookie
   const token = createJWT({
-    name: loggedInUser.name,
+    userId: loggedInUser._id,
     role: loggedInUser.role,
   });
-  console.log(token);
+  // one day in sec
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie("token", token, {
+    httpOnly: true,
+    expiresIn: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
 
-  res.status(StatusCodes.OK).send("login");
+  res.status(StatusCodes.OK).json({ msg: "user logged in!" });
 };
--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export const createJWT = (payload) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.EXPIRES_IN,
+  });
   return token;
 };
