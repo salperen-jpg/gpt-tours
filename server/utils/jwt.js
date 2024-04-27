ba8e16acc70f5adc59cca090ae5cc85d52a43959
Salih Alperen <sal.alperen1@gmail.com>
2024-04-27 15:20:57 +0200

    Add auth middleware

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -6,3 +6,9 @@ export const createJWT = (payload) => {
   });
   return token;
 };
+
+export const verifyJWT = (token) => {
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  console.log(decoded);
+  return decoded;
+};
