08ba732e7b0bba53a4467d35e8b6b4e669b719f2
Salih Alperen <sal.alperen1@gmail.com>
2024-05-03 20:55:47 +0200

    Remove log

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,7 +3,6 @@ import { verifyJWT } from "../utils/jwt.js";
 
 export const authMiddleware = async (req, res, next) => {
   const { token } = req.cookies;
-  console.log(token);
   if (!token) throw new UnauthenticatedError("authentication invalid");
   try {
     const { userId, role } = verifyJWT(token);
