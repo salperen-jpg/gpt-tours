8e12aafe845d9a3c66783e7eb2b9a65ca628f811
Salih Alperen <sal.alperen1@gmail.com>
2024-05-05 18:18:58 +0200

    Add shadcn-ui configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-7xl">Hi there!</h1>
+      <Button type="button">click me!</Button>
     </>
   );
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +11,11 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
