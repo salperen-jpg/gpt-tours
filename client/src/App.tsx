12058716c184c157611109b43bc5ad216dbb162d
Salih Alperen <sal.alperen1@gmail.com>
2024-05-09 22:55:52 +0200

    Add error page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {
+  Error,
   Landing,
   Login,
+  Profile,
   Register,
   SharedLayout,
   SingleTour,
@@ -13,26 +15,34 @@ function App() {
     {
       path: "register",
       element: <Register />,
+      errorElement: <Error />,
     },
     {
       path: "login",
       element: <Login />,
+      errorElement: <Error />,
     },
     {
       path: "/",
       element: <Landing />,
+      errorElement: <Error />,
     },
     {
       path: "dashboard",
       element: <SharedLayout />,
+      errorElement: <Error />,
       children: [
         {
-          path: "tours",
+          index: true,
           element: <Tours />,
         },
         {
           path: "tours/:id",
           element: <SingleTour />,
+        },
+        {
+          path: "profile",
+          element: <Profile />,
         },
       ],
     },
--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,4 +1,27 @@
+import { useRouteError, isRouteErrorResponse, Link } from "react-router-dom";
+import errorImg from "../assets/errorImg.svg";
+import { Button } from "@/components/ui/button";
 const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+  if (isRouteErrorResponse(error) && error.status === 404) {
+    return (
+      <main className="min-h-[100vh] grid place-items-center">
+        <div className="text-center px-8">
+          <img src={errorImg} alt="error" className="max-w-md mb-6" />
+          <h1 className="text-3xl font-bold tracking-tight sm:text-5xl mb-6 text-primary">
+            Page not found!
+          </h1>
+          <p className="tracking-wide mb-6">
+            Sorry, we could not find the page you are looking for
+          </p>
+          <Button asChild size="lg" variant="secondary">
+            <Link to="/dashboard">Go back home</Link>
+          </Button>
+        </div>
+      </main>
+    );
+  }
   return <div>Error</div>;
 };
 export default Error;
