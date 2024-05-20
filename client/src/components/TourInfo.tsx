9ce62eaea3849dd03d7a14be08543ca1532261b7
Salih Alperen <sal.alperen1@gmail.com>
2024-05-20 18:57:44 +0200

    Add latest tour-info

--- client/src/components/TourInfo.tsx
+++ client/src/components/TourInfo.tsx
@@ -1,30 +1,44 @@
 import { Tour, TourResponse } from "@/utils";
+import { MdTour } from "react-icons/md";
 
 type TourInfoProps = {
   tourInfo: TourResponse;
 };
 
 const TourInfo = ({ tourInfo }: TourInfoProps) => {
-  const { city, description, stops, title, country } = tourInfo;
-  // re-write css after adding the image
+  const { city, description, stops, title, country, image } = tourInfo;
   return (
-    <section>
-      <div>
-        <h1>
-          {city}/{country}
-        </h1>
-        <h2 className="text-primary">{title}</h2>
-        <p>{description}</p>
+    <>
+      <h1 className="mb-8 text-primary text-2xl tracking-wide">
+        {city}-{country}
+      </h1>
+      <section className="grid gap-8 lg:grid-cols-2 gap-x-12">
+        {image && (
+          <img
+            src={image}
+            alt={country}
+            className="rounded-lg bg-opacity-85 lg:max-h-[500px] lg:min-w-[500px] lg:object-cover"
+          />
+        )}
         <div>
-          <span>Activities</span>
-          <ul>
-            {stops.map((act) => {
-              return <li key={act}>{act}</li>;
-            })}
-          </ul>
+          <h1 className="text-primary mb-4 font-semibold">{title}</h1>
+          <p className="leading-7 mb-4">{description}</p>
+          <div>
+            <span className="block text-primary mb-4">Activities</span>
+            <ul>
+              {stops.map((act) => {
+                return (
+                  <li key={act} className="flex items-center gap-x-4 mb-4">
+                    <MdTour className="text-green-500 size-16" />
+                    <span>{act}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 export default TourInfo;
