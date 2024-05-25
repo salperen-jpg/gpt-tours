2c81f0633b07f7500f5ae6ab0fc77c78853aea0c
salperen <sal.alperen1@gmail.com>
2024-05-25 14:32:28 +0200

    Remove white spaces

--- client/src/components/SectionTitle.tsx
+++ client/src/components/SectionTitle.tsx
@@ -2,9 +2,8 @@ import { CgChevronRight } from "react-icons/cg";
 
 const SectionTitle = ({ title }: { title: string }) => {
   return (
-    <div className="text-center mb-12 flex justify-center items-center gap-x-8">
+    <div className="text-center mb-16 flex justify-center items-center gap-x-8">
       <CgChevronRight className="text-4xl section-icon" />
-
       <h2 className="font-['Caveat'] text-2xl sm:text-3xl lg:text-5xl">
         {title}
       </h2>
