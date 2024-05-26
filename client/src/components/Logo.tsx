a4b98a6d302a97dc3cfafbd717d6301cecbd3828
salperen <sal.alperen1@gmail.com>
2024-05-26 22:25:23 +0200

    Add temporary logo

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,4 +1,11 @@
 const Logo = () => {
-  return <div>Coming up</div>;
+  return (
+    <h3 className="logo font-[Caveat] text-4xl font-semibold ">
+      T
+      <span className="font-extrabold bg-clip-text bg-gradient-to-r from-violet-300 to-fuchsia-300 text-transparent">
+        {"<"}OURS{">"}
+      </span>
+    </h3>
+  );
 };
 export default Logo;
