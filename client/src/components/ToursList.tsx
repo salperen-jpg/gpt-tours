784c8448d4b2537dde4b1690ba38866beb6a0c33
salperen <sal.alperen1@gmail.com>
2024-06-10 19:50:31 +0200

    Add if there is no tour

--- client/src/components/ToursList.tsx
+++ client/src/components/ToursList.tsx
@@ -9,8 +9,25 @@ import {
 } from "@/components/ui/card";
 import { Button } from "./ui/button";
 import { ToursData } from "@/pages/Tours";
+import { RiAlarmWarningFill } from "react-icons/ri";
+
 const ToursList = () => {
   const { tours } = useLoaderData() as ToursData;
+
+  if (tours.length < 1) {
+    return (
+      <div className="h-[30vh] grid place-items-center">
+        <div className="flex flex-col justify-center items-center gap-y-4">
+          <RiAlarmWarningFill className="text-4xl warning" />
+          <p className="text-xl">It seems you don't have any tours!</p>
+          <Button asChild>
+            <Link to="./newTour">Create new tour</Link>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <section className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
       {tours.map((tour) => {
