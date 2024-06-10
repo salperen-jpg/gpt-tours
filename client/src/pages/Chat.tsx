e49c61fd23ef33dd4e2b8cc6643038281084ba4f
salperen <sal.alperen1@gmail.com>
2024-06-10 20:37:56 +0200

    Fix react key warning

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -39,14 +39,14 @@ const Chat = () => {
       <TokenAmount />
       <main className="min-h-[calc(100vh-15rem)] grid grid-rows-[1fr,auto] ">
         <div className="h-[calc(100vh-19rem)] overflow-y-auto scroll-bar-chat">
-          {messages.map((message) => {
+          {messages.map((message, i) => {
             const { content, role } = message;
             const classes =
               role === "user"
                 ? "flex flex-col items-end p-4"
                 : "py-4 flex-col   max-w-2xl leading-6";
             return (
-              <article className={classes}>
+              <article key={i} className={classes}>
                 <span className="block uppercase mb-4 text-primary">
                   {role}
                 </span>
