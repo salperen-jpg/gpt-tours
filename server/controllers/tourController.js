e8533aeb5b1a4c639dda0534ab3e306c7fd7b342
salperen <sal.alperen1@gmail.com>
2024-06-15 20:47:54 +0200

    Add query params to tours controller

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -3,8 +3,63 @@ import { StatusCodes } from "http-status-codes";
 
 // GET TOURS
 const getAllTours = async (req, res) => {
-  const tours = await Tour.find({ createdBy: req.user.userId });
-  res.status(StatusCodes.OK).json({ tours });
+  console.log(req.query);
+  // start with query params;
+  const { title, city, country, sort } = req.query;
+  const queryParamsObject = {
+    createdBy: req.user.userId,
+  };
+  // if city  passed
+  if (title) {
+    queryParamsObject.$or = [
+      {
+        title: { $regex: title, $options: "i" },
+      },
+    ];
+  }
+  if (city) {
+    queryParamsObject.$or = [
+      {
+        city: { $regex: city, $options: "i" },
+      },
+    ];
+  }
+  // if country passed
+  if (country) {
+    queryParamsObject.$or = [
+      {
+        country: { $regex: country, $options: "i" },
+      },
+    ];
+  }
+  // sorting
+  const sortOptions = {
+    "a-z": "title",
+    "z-a": "-title",
+    newest: "-createdAt",
+    oldest: "createdAt",
+  };
+
+  // pagination
+  const page = Number(req.query.page) || 1;
+  // tours per page
+  const limit = 9;
+  const skip = (page - 1) * limit;
+
+  const tours = await Tour.find(queryParamsObject)
+    .sort(sortOptions[sort ?? "a-z"])
+    .skip(skip)
+    .limit(limit);
+
+  const numOfTours = await Tour.countDocuments(queryParamsObject);
+
+  const totalPageNumber = Math.ceil(numOfTours / limit);
+
+  // const toursNumber=
+
+  res
+    .status(StatusCodes.OK)
+    .json({ numOfTours, currentPage: page, totalPageNumber, tours });
 };
 
 // POST TOUR
