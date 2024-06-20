537aabe798d99db3235d50ae11db3d57cca55776
salperen <sal.alperen1@gmail.com>
2024-06-20 22:08:26 +0200

    Add href to logo

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Logo = () => {
   return (
-    <h3 className="logo font-[Caveat] text-4xl font-semibold ">
-      T
-      <span className="font-extrabold bg-clip-text bg-gradient-to-r from-violet-300 to-fuchsia-300 text-transparent">
-        {"<"}OURS{">"}
-      </span>
-    </h3>
+    <Link to="/dashboard">
+      <h3 className="logo font-[Caveat] text-4xl font-semibold ">
+        T
+        <span className="font-extrabold bg-clip-text bg-gradient-to-r from-violet-300 to-fuchsia-300 text-transparent">
+          {"<"}OURS{">"}
+        </span>
+      </h3>
+    </Link>
   );
 };
 export default Logo;
