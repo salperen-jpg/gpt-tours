0d944a7b80583be728165404f2c24fab929501ac
salperen <sal.alperen1@gmail.com>
2024-06-20 22:08:38 +0200

    Add logo to footer

--- client/src/components/LandingFooter.tsx
+++ client/src/components/LandingFooter.tsx
@@ -1,8 +1,13 @@
+import Logo from "./Logo";
+
 const LandingFooter = () => {
   return (
-    <footer className="py-4 flex flex-col justify-center items-center">
-      <span>&copy; {new Date().getFullYear()}</span>
-      <span> All rights reserved </span>
+    <footer className="py-4 flex flex-col justify-center items-center gap-y-2">
+      <Logo />
+      <div className="text-center">
+        <p>&copy; {new Date().getFullYear()}</p>
+        <p> All rights reserved </p>
+      </div>
     </footer>
   );
 };
